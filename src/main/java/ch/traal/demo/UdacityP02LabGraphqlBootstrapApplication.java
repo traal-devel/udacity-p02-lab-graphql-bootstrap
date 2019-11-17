662b33af78919dcfb7476773f44d9fc7cd1048fb
devel@traal.ch <devel@traal.ch>
2019-11-17 16:57:10 +0100

    chore: First revision

---
+++ src/main/java/ch/traal/demo/UdacityP02LabGraphqlBootstrapApplication.java
@@ -0,0 +1,13 @@
+package ch.traal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdacityP02LabGraphqlBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdacityP02LabGraphqlBootstrapApplication.class, args);
+	}
+
+}
